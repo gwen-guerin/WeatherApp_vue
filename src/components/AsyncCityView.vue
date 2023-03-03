77a58246c45b5f0574cd97ca4ccca952a1b5f730
Gwen Guérin <gwenael.guerin@protonmail.com>
2023-03-03 11:35:52 +0100

    first vue app

---
+++ src/components/AsyncCityView.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="mb-10 flex flex-col flex-1 items-center">
+    <!-- BANNER -->
+    <div
+      v-if="route.query.preview"
+      class="text-white p-4 bg-weather-secondary w-full text-center"
+    >
+      <p>
+        You're currently previewing this city, click '+' to start tracking this
+        city
+      </p>
+    </div>
+    <!-- Weather overview -->
+    <div class="flex flex-col items-center text-white py-12">
+      <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
+      <p class="text-sm mb-12">
+        {{
+          new Date(weatherData.currentTime).toLocaleDateString('fr', {
+            weekday: 'short',
+            day: '2-digit',
+            month: 'long',
+          })
+        }}
+        {{
+          new Date(weatherData.currentTime).toLocaleTimeString('fr', {
+            timeStyle: 'short',
+          })
+        }}
+      </p>
+      <p class="text-8xl mb-8">
+        {{ Math.round(weatherData.current.temp) }}&deg;
+      </p>
+      <p>
+        Ressenti
+        {{ Math.round(weatherData.current.feels_like) }}&deg;
+      </p>
+      <p class="capitalize">
+        {{ weatherData.current.weather[0].description }}
+      </p>
+      <img
+        class="w-[150px] h-auto"
+        :src="`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`"
+        alt="/"
+      />
+    </div>
+    <hr class="border-white/10 border w-full" />
+
+    <!-- Hourly Weather -->
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">Hourly Weather</h2>
+        <div
+          class="flex gap-10 scrollbar scrollbar-thumb-weather-secondary scrollbar-track-weather-primary overflow-x-scroll"
+        >
+          <div
+            v-for="hourData in weatherData.hourly"
+            :key="hourData.dt"
+            class="mb-6 flex flex-col gap-4 items-center"
+          >
+            <p class="whitespace-nowrap text-md">
+              {{
+                new Date(hourData.currentTime).toLocaleTimeString('fr', {
+                  hour: 'numeric',
+                })
+              }}
+            </p>
+            <img
+              class="w-auto h-[50px] object-cover"
+              :src="`http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`"
+              alt="/"
+            />
+            <p class="text-xl">{{ Math.round(hourData.temp) }}&deg;</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr class="border-white/10 border w-full" />
+
+    <!-- Weekly Weather -->
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">7 Days Forecast</h2>
+        <div
+          v-for="day in weatherData.daily"
+          :key="day.dt"
+          class="flex items-center"
+        >
+          <p class="flex-1">
+            {{
+              new Date(day.dt * 1000).toLocaleDateString('fr', {
+                weekday: 'long',
+              })
+            }}
+          </p>
+          <img
+            class="w-[50px] h-[50px] object-cover"
+            :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
+            alt="/"
+          />
+          <div class="flex gap-2 flex-1 justify-end">
+            <p>Max: {{ Math.round(day.temp.max) }}&deg;</p>
+            <p>Min: {{ Math.round(day.temp.min) }}&deg;</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      @click="removeCity"
+      class="flex items-center gap-2 py-12 text-white cursor-pointer duration-150 hover:text-red-500"
+    >
+      <i class="fa-solid fa-trash"></i>
+      <p>Remove City</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const getWeatherData = async () => {
+  try {
+    const weatherData = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=metric`
+    );
+    // cal current date & time
+    const localOffset = new Date().getTimezoneOffset() * 60000;
+    const utc = weatherData.data.current.dt * 1000 + localOffset;
+    weatherData.data.currentTime =
+      utc + 1000 * weatherData.data.timezone_offset;
+    // cal hourly weather offset
+    weatherData.data.hourly.forEach((hour) => {
+      const utc = hour.dt * 1000 + localOffset;
+      hour.currentTime = utc + 1000 * weatherData.data.timezone_offset;
+    });
+
+    await new Promise((res) => setTimeout(res, 1000));
+    
+    return weatherData.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+const weatherData = await getWeatherData();
+const router = useRouter();
+const removeCity = () => {
+  const cities = JSON.parse(localStorage.getItem('savedCities'));
+  const updatedCities = cities.filter((city) => city.id !== route.query.id);
+  localStorage.setItem('savedCities', JSON.stringify(updatedCities));
+  router.push({
+    name: 'home',
+  });
+};
+</script>
